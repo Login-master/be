4a443953a9d6819e8f6793f7585548ffb4d8e6b8
gichan222 <>
2024-10-07 16:16:52 +0900

    feat : UserRepository findAll 메서드 추가

--- spring-security-token-login/src/main/java/longrun/springsecuritytokenlogin/repository/UserRepository.java
+++ spring-security-token-login/src/main/java/longrun/springsecuritytokenlogin/repository/UserRepository.java
@@ -4,6 +4,7 @@ import longrun.springsecuritytokenlogin.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -11,5 +12,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUserId(String userId);
     Optional<User> findByEmail(String email);
+
+    List<User> findAll();
 
 }
