966f8c747003ffebd802a989dd6058a232519907
longrunpc <pcjmo4050@gmail.com>
2024-10-09 21:24:24 +0900

    feat : 에러코드 추가(INVALID_INPUT_VALUE)

--- spring-security-session-login/src/main/java/longrun/springsecuritysessionlogin/exception/ErrorCode.java
+++ spring-security-session-login/src/main/java/longrun/springsecuritysessionlogin/exception/ErrorCode.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
     ACCESS_DENIED("접근 권한이 없습니다."),
+    INVALID_INPUT_VALUE("유효성 검증에 실패하였습니다."),
     UNAUTHORIZED("인증정보가 존재하지 않습니다."),
     USER_NOT_FOUND("회원정보가 없습니다"),
     USER_ID_EXIST("이미 존재하는 아이디입니다."),
